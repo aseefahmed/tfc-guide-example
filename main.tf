9b3bc6e5ec958053bb7329319526451aa3190b19
Aseef Ahmed <aseefahmed@gmail.com>
2020-09-15 19:15:57 +1200

    4

--- main.tf
+++ main.tf
@@ -12,7 +12,9 @@ resource "random_pet" "table_name" {}
 
 resource "aws_dynamodb_table" "tfc_example_table" {
   name = "${var.db_table_name}-${random_pet.table_name.id}"
-
+  tags = {
+    "username" = var.tag_user_name
+  }
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
   hash_key       = "UUID"
@@ -24,5 +26,5 @@ resource "aws_dynamodb_table" "tfc_example_table" {
 }
 
 resource "aws_s3_bucket" "buc1" {
-  
+
 }
